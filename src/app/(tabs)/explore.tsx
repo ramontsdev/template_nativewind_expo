a1d59e3223faf6e3413ff8e87530ec0daee417c2
Ramon Nunes <ramontsdev@gmail.com>
2024-06-14 09:49:09 -0300

    chore: nativewind configure

--- src/app/(tabs)/explore.tsx
+++ src/app/(tabs)/explore.tsx
@@ -3,8 +3,8 @@ import { Text, View } from 'react-native';
 
 export default function TabTwoScreen() {
   return (
-    <View>
-      <Text>
+    <View className='flex flex-1 justify-center items-center'>
+      <Text className='text-lg font-semibold'>
         TabTwoScreen
       </Text>
     </View>
--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>
+    <View className='flex flex-1 justify-center items-center'>
+      <Text className='text-lg font-semibold'>
         HomeScreen
       </Text>
     </View>
